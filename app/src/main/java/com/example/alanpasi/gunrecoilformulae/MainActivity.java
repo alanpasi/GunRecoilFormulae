bd82105a7f998a5e5d8d103e382657bc919e2017
alanpasi <alanpasi@gmail.com>
2016-11-27 12:36:18 -0200

    Initial commit

---
+++ app/src/main/java/com/example/alanpasi/gunrecoilformulae/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alanpasi.gunrecoilformulae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
